05a7bf6ac02eb06bddc659f4c7bb74646b2ee614
GabrielReyes136 <49967790+GabrielReyes136@users.noreply.github.com>
2022-07-18 04:30:27 -0700

    Update altar_of_net_config.java

--- src/main/java/com/altar_of_net/altar_of_net_config.java
+++ src/main/java/com/altar_of_net/altar_of_net_config.java
@@ -4,9 +4,10 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("altar-of-net-deprecated")
+@ConfigGroup("deprecated")
 public interface altar_of_net_config extends Config
 {
+/*
 	@ConfigItem(
 		keyName = "This plug-in can only be used if you are part of the clan: Altar of NET",
 		name = "This plug-in can only be used if you are part of the clan: Altar of NET",
@@ -16,4 +17,5 @@ public interface altar_of_net_config extends Config
 	{
 		return "This plug-in can only be used if you are part of the clan: Altar of NET";
 	}
+*/
 }
